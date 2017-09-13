0f0e0abde583533a3fbe4b16cd2ddf871a879a5d
Richard <richard_c_adams@hotmail.com>
2017-09-13 12:18:32 -0700

    adding client tests

---
+++ src/client/tests/index.test.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+const mockRender = jest.fn();
+
+jest.mock("react-dom", () => ({
+    render: mockRender,
+}));
+
+const mockApp = () => <div></div>;
+
+jest.mock("../../App", () => ({
+    default: mockApp,
+}));
+
+const mockStore = { store: true };
+const mockConfigureStore = jest.fn(() => mockStore);
+
+jest.mock("../../App/state", () => ({
+    configureStore: mockConfigureStore,
+}));
+
+const initialState = { state: true };
+(window as any).__INITIAL_STATE__ = initialState;
+
+import "../";
+import App from "../../App";
+
+it("should call configureStore with initial state", () => {
+
+    expect(mockConfigureStore).toBeCalledWith(initialState);
+});
+
+it("should call ReactDOM.render with App", () => {
+
+    expect(mockRender.mock.calls[0][0]).toEqual(<App store={mockStore as any} />);
+});
